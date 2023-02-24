3a0d8224416f53a07f46aaa197cf15bc67d4ee30
Yangha Kim <yangha.kim.96@gmail.com>
2023-02-24 15:29:19 -0500

    update db json

--- src/app/models/Item.ts
+++ src/app/models/Item.ts
@@ -1,10 +1,12 @@
 export interface Item {
     id: number,
     fund: string,
-    pending_actions: null,
+    pending_actions: string,
     daily_book_pl: string,
     mtd_book_pl: string,
     ytd_book_pl: string,
     end_book_nav: string,
-    client: string
+    client: string,
+    fund_client: string,
+    date: string,
 }
